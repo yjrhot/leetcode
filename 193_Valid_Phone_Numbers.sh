3399ec8504b68872e2d5147be9dc7c3929406a40
yujianrong <yujianrong@mlogcn.com>
2021-01-06 18:15:53 +0800

    又开始刷leetcode了

---
+++ 193_Valid_Phone_Numbers.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+while read line;
+do
+    if [[ "$line" =~ [0-9]{3}-[0-9]{3}-[0-9]{4} ]] || [[ "$line" =~ \([0-9]{3}\)\ [0-9]{3}-[0-9]{4}  ]];then
+        echo $line
+    fi
+done < file.txt
